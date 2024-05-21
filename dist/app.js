4001b720ab17b28038c59a21172732e005d79c6d
miavai649 <mahmudulhaquenoor@gmail.com>
2024-05-22 00:23:08 +0600

    create order function created

--- dist/app.js
+++ dist/app.js
@@ -6,6 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
 const product_route_1 = require("./app/modules/products/product.route");
+const order_route_1 = require("./app/modules/orders/order.route");
 const app = (0, express_1.default)();
 // all parsers
 app.use(express_1.default.json());
@@ -13,6 +14,7 @@ app.use((0, cors_1.default)());
 // APPLICATION ROUTES
 // ---------- Product Routes -----------
 app.use("/api/products", product_route_1.ProductRouter);
+app.use("/api/orders", order_route_1.OrderRouter);
 app.get("/", (req, res) => {
     const a = "hello world";
     res.send(a);
--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
 import { ProductRouter } from "./app/modules/products/product.route";
+import { OrderRouter } from "./app/modules/orders/order.route";
 
 const app: Application = express();
 
@@ -12,6 +13,7 @@ app.use(cors());
 
 // ---------- Product Routes -----------
 app.use("/api/products", ProductRouter);
+app.use("/api/orders", OrderRouter);
 
 app.get("/", (req: Request, res: Response) => {
   const a = "hello world";
