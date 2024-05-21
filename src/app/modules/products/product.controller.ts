466642da131bf80ecc9465cbf5e1385526d7b844
miavai649 <mahmudulhaquenoor@gmail.com>
2024-05-21 11:09:26 +0600

    get all products service function created

--- src/app/modules/products/product.controller.ts
+++ src/app/modules/products/product.controller.ts
@@ -1,26 +1,44 @@
-import { Request, Response } from "express";
-import { ProductServices } from "./product.service";
+import { Request, Response } from 'express'
+import { ProductServices } from './product.service'
 
 const createProduct = async (req: Request, res: Response) => {
   try {
-    const productData = req.body;
+    const productData = req.body
 
-    const result = await ProductServices.createProductIntoDB(productData);
+    const result = await ProductServices.createProductIntoDB(productData)
 
     res.status(200).json({
       success: true,
-      message: "Product created successfully!",
-      data: result,
-    });
+      message: 'Product created successfully!',
+      data: result
+    })
   } catch (error) {
     res.status(500).json({
       success: false,
-      message: "Failed to create product. Please try again later.",
-      error: error,
-    });
+      message: 'Failed to create product. Please try again later.',
+      error: error
+    })
   }
-};
+}
+
+const getAllProduct = async (req: Request, res: Response) => {
+  try {
+    const result = await ProductServices.getAllProductsFromDB()
+
+    res.status(200).json({
+      success: true,
+      message: 'Products fetched successfully!',
+      data: result
+    })
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: 'Failed to fetch products. Please try again later.',
+      error: error
+    })
+  }
+}
 
 export const ProductControllers = {
-  createProduct,
-};
+  createProduct
+}
--- src/app/modules/products/product.service.ts
+++ src/app/modules/products/product.service.ts
@@ -1,11 +1,17 @@
-import { TProduct } from "./product.interface";
-import { Product } from "./product.model";
+import { TProduct } from './product.interface'
+import { Product } from './product.model'
 
 const createProductIntoDB = async (product: TProduct) => {
-  const result = await Product.create(product);
-  return result;
-};
+  const result = await Product.create(product)
+  return result
+}
+
+const getAllProductsFromDB = async () => {
+  const result = await Product.find()
+  return result
+}
 
 export const ProductServices = {
   createProductIntoDB,
-};
+  getAllProductsFromDB
+}
