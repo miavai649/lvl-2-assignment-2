8ae45a1ee8ab8cb6a50414fc7d12781b5e896455
miavai649 <mahmudulhaquenoor@gmail.com>
2024-05-21 09:34:40 +0600

    product modal created

--- src/app/modules/products/product.interface.ts
+++ src/app/modules/products/product.interface.ts
@@ -1,19 +1,19 @@
-export type Variant = {
+export type TVariant = {
   type: string
   value: string
 }
 
-export type Inventory = {
+export type TInventory = {
   quantity: number
   inStock: boolean
 }
 
-export type Product = {
+export type TProduct = {
   name: string
   description: string
   price: number
   category: string
   tags: string[]
-  variants: Variant[]
-  inventory: Inventory
+  variants: TVariant[]
+  inventory: TInventory
 }
